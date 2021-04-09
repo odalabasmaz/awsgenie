3bf3ed1a320fabe8ad09996e508b670ae41a9c78
Celal Emre Çiçek <celal.emre@opsgenie.com>
2021-04-09 13:55:22 +0300

    Add TerminateIamPolicyResourcesTest.

---
+++ aws-resource-terminator/src/test/java/com/atlassian/awsterminator/terminate/TerminateIamPolicyResourcesTest.java
@@ -0,0 +1,95 @@
+package com.atlassian.awsterminator.terminate;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.model.DeletePolicyRequest;
+import com.atlassian.awstool.terminate.AWSResource;
+import com.atlassian.awstool.terminate.FetchResourceFactory;
+import com.atlassian.awstool.terminate.FetchResources;
+import com.atlassian.awstool.terminate.iamPolicy.IAMPolicyResource;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * @author Celal Emre CICEK
+ * @version 9.04.2021
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class TerminateIamPolicyResourcesTest {
+    private static final String TEST_REGION = "us-west-2";
+    private static final String TEST_TICKET = "TEST-TICKET";
+
+    private static final List<String> TEST_RESOURCES = new ArrayList<String>() {{
+        add("policy1");
+        add("policy2");
+        add("policy3");
+    }};
+
+    private static final List<AWSResource> TEST_FETCHED_RESOURCES = new ArrayList<AWSResource>() {{
+        add(new IAMPolicyResource()
+                .setResourceName("policy1")
+                .setLastUsedDate(DateTime.now().minus(TimeUnit.DAYS.toMillis(8)).toDate()));
+        add(new IAMPolicyResource()
+                .setResourceName("policy2")
+                .setLastUsedDate(DateTime.now().minus(TimeUnit.DAYS.toMillis(5)).toDate()));
+    }};
+
+    @Mock
+    private AmazonIdentityManagement iamClient;
+
+    @Mock
+    private AWSCredentialsProvider credentialsProvider;
+
+    @Mock
+    private FetchResourceFactory fetchResourceFactory;
+
+    @Mock
+    private FetchResources fetchResources;
+
+    private TerminateIamPolicyResources terminateIamPolicyResources;
+
+    @Before
+    public void setUp() throws Exception {
+        this.terminateIamPolicyResources = new TerminateIamPolicyResources(credentialsProvider);
+        terminateIamPolicyResources.setIAMClient(iamClient);
+        terminateIamPolicyResources.setFetchResourceFactory(fetchResourceFactory);
+
+        when(fetchResourceFactory.getFetcher("iamPolicy", credentialsProvider))
+                .thenReturn(fetchResources);
+        doReturn(TEST_FETCHED_RESOURCES)
+                .when(fetchResources).fetchResources(eq(TEST_REGION), eq(TEST_RESOURCES), org.mockito.Mockito.any(List.class));
+    }
+
+    @Test
+    public void terminateResourcesWithApply() throws Exception {
+        terminateIamPolicyResources.terminateResource(TEST_REGION, "iamPolicy", TEST_RESOURCES, TEST_TICKET, true);
+
+        ArgumentCaptor<DeletePolicyRequest> captor = ArgumentCaptor.forClass(DeletePolicyRequest.class);
+        verify(iamClient).deletePolicy(captor.capture());
+        DeletePolicyRequest actualRequest = captor.getValue();
+        assertThat(actualRequest.getPolicyArn(), is(equalTo("policy1")));
+        verifyNoMoreInteractions(iamClient);
+    }
+
+    @Test
+    public void terminateResourcesWithoutApply() throws Exception {
+        terminateIamPolicyResources.terminateResource(TEST_REGION, "iamPolicy", TEST_RESOURCES, TEST_TICKET, false);
+        verifyZeroInteractions(iamClient);
+    }
+}
